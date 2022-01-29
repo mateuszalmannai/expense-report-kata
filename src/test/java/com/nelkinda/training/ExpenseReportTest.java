83d0120249b2f9d05499f8fa66743954ab91b443
mateusz <mateuszalmannai@googlemail.com>
2022-01-29 18:21:47 +0100

    test big report, all cases

--- src/test/java/com/nelkinda/training/ExpenseReportTest.java
+++ src/test/java/com/nelkinda/training/ExpenseReportTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 public class ExpenseReportTest {
 
@@ -16,5 +17,32 @@ public class ExpenseReportTest {
     ExpenseReport report = new ExpenseReport();
     report.printReport(Collections.emptyList(), new Date(0));
     Approvals.verify(output);
+  }
+
+
+  @Test
+  void bigReportTest() {
+    ByteArrayOutputStream output = new ApprovalUtilities().writeSystemOutToStringBuffer();
+    ExpenseReport report = new ExpenseReport();
+
+    List<Expense> expenses = List.of(
+        createExpense(ExpenseType.DINNER, 5000),
+        createExpense(ExpenseType.DINNER, 5001),
+        createExpense(ExpenseType.BREAKFAST, 1000),
+        createExpense(ExpenseType.BREAKFAST, 1001),
+        createExpense(ExpenseType.CAR_RENTAL, 50),
+        createExpense(ExpenseType.CAR_RENTAL, Integer.MAX_VALUE)
+     );
+
+
+    report.printReport(expenses, new Date(0));
+    Approvals.verify(output);
+  }
+
+  private Expense createExpense(ExpenseType type, int amount) {
+    Expense expense = new Expense();
+    expense.type = type;
+    expense.amount = amount;
+    return expense;
   }
 }
