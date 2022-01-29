24fa728254648ff1a59a2f550d61452c55170e23
mateusz <mateuszalmannai@googlemail.com>
2022-01-29 17:35:22 +0100

    start here

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Another excellent Kata using approval tests; just what I was looking for
+
+# The Challenge 
+- Add a new expense type called `LUNCH` 
+- with a limit of 2000
+
+# Solution
+- There are a lot of places in the code that we would have to change 
--- src/main/java/com/nelkinda/training/ExpenseReport.java
+++ src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -1,25 +1,43 @@
 package com.nelkinda.training;
 
 import java.util.Date;
+import java.util.List;
 
 public class ExpenseReport {
-  public void printReport(Expenses expenses) {
-    printReport(expenses, new Date());
-  }
+  public void printReport(List<Expense> expenses) {
+    int total = 0;
+    int mealExpenses = 0;
 
-  public void printReport(Expenses expenses, Date date) {
-    System.out.println("Expenses " + date);
+    System.out.println("Expenses " + new Date());
 
     for (Expense expense : expenses) {
-      printSingleExpense(expense);
+      if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST) {
+        mealExpenses += expense.amount;
+      }
+
+      String expenseName = "";
+      switch (expense.type) {
+        case DINNER:
+          expenseName = "Dinner";
+          break;
+        case BREAKFAST:
+          expenseName = "Breakfast";
+          break;
+        case CAR_RENTAL:
+          expenseName = "Car Rental";
+          break;
+      }
+
+      String mealOverExpensesMarker =
+          expense.type == ExpenseType.DINNER && expense.amount > 5000 ||
+          expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
+
+      System.out.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
+
+      total += expense.amount;
     }
 
-    System.out.println("Meal expenses: " + expenses.getMealExpenses());
-    System.out.println("Total expenses: " + expenses.getTotal());
-  }
-
-  private void printSingleExpense(Expense expense) {
-    String mealOverExpensesMarker = expense.isOverLimit() ? "X" : " ";
-    System.out.println(expense.getName() + "\t" + expense.getAmount() + "\t" + mealOverExpensesMarker);
+    System.out.println("Meal expenses: " + mealExpenses);
+    System.out.println("Total expenses: " + total);
   }
 }
