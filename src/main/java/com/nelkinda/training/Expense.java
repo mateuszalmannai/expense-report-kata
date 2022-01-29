b06f8583bb3f0cb90c482719ebe7140106ff23f8
mateusz <mateuszalmannai@googlemail.com>
2022-01-29 20:43:11 +0100

    refactor: encapsulate Expense

--- src/main/java/com/nelkinda/training/Expense.java
+++ src/main/java/com/nelkinda/training/Expense.java
@@ -3,4 +3,20 @@ package com.nelkinda.training;
 public class Expense {
   ExpenseType type;
   int amount;
+
+  String getName() {
+    return type.getName();
+  }
+
+  boolean isMeal() {
+    return type.isMeal();
+  }
+
+  boolean isOverLimit() {
+    return amount > type.getLimit();
+  }
+
+  int getAmount() {
+    return amount;
+  }
 }
